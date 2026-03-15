0ed874d3bd60b435c964ff7330fbfb290261f5e9
Raushan kumar <sahilsingh22111@gmail.com>
2026-03-15 15:31:02 +0530

    Update Day2.py

--- Day2.py
+++ Day2.py
@@ -6,6 +6,13 @@ width = float(input("Enter a width"))
 area = length * width              # calculate the area of ractangle.
 print("Area of rectangle :",area)
 
+# This is the second problem.
+
+celsius = float(input("Enter temperature in Celsius: "))
+
+fahrenheit = (celsius * 9/5) + 32
+
+print("Temperature in Fahrenheit:", fahrenheit)
 
 
 
