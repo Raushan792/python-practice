f3a0c0fa1b8f27e725a87d6de0fff0e0fb1fd94d
Raushan kumar <sahilsingh22111@gmail.com>
2026-03-15 15:23:20 +0530

    Create Day2.py

---
+++ Day2.py
@@ -0,0 +1,11 @@
+# Day 2 python prablem.
+# Calculate the area of ractangle.
+
+length = float(input("Enter a length"))
+width = float(input("Enter a width"))
+area = length * width              # calculate the area of ractangle.
+print("Area of rectangle :",area)
+
+
+
+
