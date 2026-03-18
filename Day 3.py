dc6b829e63272f2c2984576d67838665f6bc9061
Raushan kumar <sahilsingh22111@gmail.com>
2026-03-18 15:54:15 +0530

    Update Day 3.py

--- Day 3.py
+++ Day 3.py
@@ -7,4 +7,15 @@ if numbers % 2 == 0:
 else:
   print("This is odd numbers")
               
-              
+# Problem :- largest of three numbers.
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+if a >= b and a >= c:
+    print("Largest:", a)
+elif b >= a and b >= c:
+    print("Largest:", b)
+else:
+    print("Largest:", c)
