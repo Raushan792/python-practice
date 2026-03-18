9916bb013a9553aa520b14c89ff5a27e03c4a3ca
Raushan kumar <sahilsingh22111@gmail.com>
2026-03-18 15:59:10 +0530

    Create Day4.py

---
+++ Day4.py
@@ -0,0 +1,23 @@
+# python question :- positive , Negative and Zero.
+
+num = float(input("Enter a number: "))
+
+if num > 0:
+    print("Positive")
+elif num < 0:
+    print("Negative")
+else:
+    print("Zero")
+
+# Question 2 :- Grade Calculator.
+
+marks = int(input("Enter your marks: "))
+
+if marks >= 90:
+    print("Grade: A")
+elif marks >= 75:
+    print("Grade: B")
+elif marks >= 50:
+    print("Grade: C")
+else:
+    print("Fail")
