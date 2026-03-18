443d1d57952caa2fcd81c320680b7774eb0ae75f
Raushan kumar <sahilsingh22111@gmail.com>
2026-03-18 15:39:27 +0530

    Create Day 3.py

---
+++ Day 3.py
@@ -0,0 +1,10 @@
+# Day 3 python problem.
+# Question even and odd numbers.
+
+numbers = int(input("Enter the  number here :-"))
+if numbers % 2 == 0:
+  print("This is even number")
+else:
+  print("This is odd numbers")
+              
+              
