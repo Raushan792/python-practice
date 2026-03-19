c178bf989479f7ddbe4103e9748c51b48a05d5c3
Raushan kumar <sahilsingh22111@gmail.com>
2026-03-19 15:18:23 +0530

    Update Day5.py

--- Day5.py
+++ Day5.py
@@ -5,3 +5,19 @@ t = int(input("Enter a number here : "))
 for i in range (1,11):
     print(t, "x", i ,"=",t*i)
 
+# Sum of to n numbers.
+
+n = int(input("Which you want : "))
+sum = 0
+for i in range (1,n+1):
+    sum = sum + i
+    print("your sum is ", sum)
+
+# Factorial of numbers.
+
+n = int(input("please tell your number : "))
+
+fact = 1
+for i in range (1,n+1):
+    fact = fact * i
+    print("your fact is ", fact)
