99b4acd19c69e7e096cdecec5992d40f6755fa0b
Raushan kumar <sahilsingh22111@gmail.com>
2026-03-23 15:02:06 +0530

    Update Day7.py

--- Day7.py
+++ Day7.py
@@ -12,3 +12,25 @@ for i in range(b):
 print("Answer:", result)
 
 # seconnd Question.
+# simple calculator.
+
+# Simple Calculator
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+op = input("Enter operator (+, -, *, /): ")
+
+if op == "+":
+    print("Result:", a + b)
+
+elif op == "-":
+    print("Result:", a - b)
+
+elif op == "*":
+    print("Result:", a * b)
+
+elif op == "/":
+    print("Result:", a / b)
+
+else:
+    print("Invalid operator")
