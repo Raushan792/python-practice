4ad298eace215b2cf75f74c0f9354c23851fa9a5
Raushan kumar <sahilsingh22111@gmail.com>
2026-03-23 14:58:19 +0530

    Create Day7.py

---
+++ Day7.py
@@ -0,0 +1,12 @@
+# Python problem.
+# Please print the power(a^b).
+
+a = int(input("Enter base: "))
+b = int(input("Enter power: "))
+
+result = 1
+
+for i in range(b):
+    result = result * a
+
+print("Answer:", result)
