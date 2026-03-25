9393fa2762951fdbdcbde7709d42ca6979a888ab
Raushan kumar <sahilsingh22111@gmail.com>
2026-03-25 14:25:40 +0530

    Update Day8.py

--- Day8.py
+++ Day8.py
@@ -9,3 +9,18 @@ print(f"negative elements are : ")
 for i in t:
     if i < 0:
         print(i)
+        
+# Question 2.        
+# Find the greatest element and print its index too.
+
+l = [23, 43, 433, 463, 742, 322, 875, 865, 887]
+
+largest = l[0]
+index = 0
+
+for i in range(len(l)):
+    if l[i] > largest:
+        largest = l[i]
+        index = i
+
+print(f"Your largest number is {largest} at index {index}")
