daed9640b8e6428db5bd85008fbc5fb3d446fbe4
Raushan kumar <sahilsingh22111@gmail.com>
2026-03-25 14:23:53 +0530

    Create Day8.py

---
+++ Day8.py
@@ -0,0 +1,11 @@
+# Python problems.
+#  print positive and negative of a list.
+t = [-23,44,-32,47,-86]
+print(f"positive elements are : ")
+for i in t:
+    if i >= 0:
+        print(i)
+print(f"negative elements are : ")
+for i in t:
+    if i < 0:
+        print(i)
