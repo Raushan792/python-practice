6f3848956a570ab0a5eba3a58a940f872b341ec7
Raushan kumar <sahilsingh22111@gmail.com>
2026-03-26 15:28:49 +0530

    Create Day9.py

---
+++ Day9.py
@@ -0,0 +1,13 @@
+# Python problem.
+# lenear search.
+# Question 1.
+
+
+# a = [22,43,54,56,46,88,77,66,99]
+# search = 46
+# for i in range(len(a)):
+#     if a[i] == search:
+#         print(f"Element found at index {i}")
+#         break
+# else:
+#     print("Element not found and not exist")
