aeed54ae58c18721ed7109ead37a5db996114a86
Raushan kumar <sahilsingh22111@gmail.com>
2026-04-01 17:49:49 +0530

    Update Day11.py

--- Day11.py
+++ Day11.py
@@ -21,3 +21,21 @@ print("Union:", A | B)          # {1, 2, 3, 4, 5, 6}
 print("Intersection:", A & B)   # {3, 4}
 print("Difference:", A - B)     # {1, 2}
 print("Symmetric Difference:", A ^ B) # {1, 2, 5, 6}
+
+# sets method.
+
+a = {2,3,4,5,8,9}
+a.add(6)
+print(a)
+
+a.remove(5)
+print(a)
+
+a.discard(12)
+print(a)
+
+a.pop(3)
+print(a)
+
+a.clear()
+print(a)
