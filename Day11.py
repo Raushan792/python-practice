2af782420f9ae24afdbc6653dd9f821d613b7a2c
Raushan kumar <sahilsingh22111@gmail.com>
2026-04-01 17:47:32 +0530

    Update Day11.py

--- Day11.py
+++ Day11.py
@@ -8,3 +8,16 @@ print(a)
 
 b = hash((3,4,5,6,7,8,9))
 print(b)
+
+# Sets Traversing.
+a = {1,2,4,6,7,8,"hello",9,3,4,5}
+for i in a:
+    print(i)
+
+A = {1, 2, 3, 4}
+B = {3, 4, 5, 6}
+
+print("Union:", A | B)          # {1, 2, 3, 4, 5, 6}
+print("Intersection:", A & B)   # {3, 4}
+print("Difference:", A - B)     # {1, 2}
+print("Symmetric Difference:", A ^ B) # {1, 2, 5, 6}
