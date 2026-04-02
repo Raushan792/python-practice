57d4189ef3ddf281ea30afa8fcb4601b6f116d14
Raushan kumar <sahilsingh22111@gmail.com>
2026-04-02 15:03:36 +0530

    Update Day12.py

--- Day12.py
+++ Day12.py
@@ -13,4 +13,15 @@ a = input("Enter a number here :")
 b = sorted(a)
 print(b)
 
+ "F.R.I.E.N.D.S"
+# 5. write a program to remove dot(.)from the following string.
+a = "S.I.N.G.H"
+r = a.replace(".","")
+print(r)
+
+# 6. write a program to check the number of occurrence
+# substring in a string.
+p = "welcome to the world"
+e = p.count("to")
+print("the occurrence is",e)
 
