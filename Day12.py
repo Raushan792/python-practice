142bfe082d8f62df77b334e1f2468cba71853d84
Raushan kumar <sahilsingh22111@gmail.com>
2026-04-02 15:01:09 +0530

    Create Day12.py

---
+++ Day12.py
@@ -0,0 +1,10 @@
+# Python Questions.
+# String method.
+
+a = "AWS.ERR.BR.RCB.CSK"
+
+# 1. write a program to separate the following string into coma (,)
+# separated values.
+a = "AWS.ERR.BR.RCB.CSK"
+print(a.split("."))
+
