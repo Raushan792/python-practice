e20918c0b7db712e57c807c46863491d3657f65c
Raushan kumar <sahilsingh22111@gmail.com>
2026-04-03 16:19:08 +0530

    Create Day13.py

---
+++ Day13.py
@@ -0,0 +1,12 @@
+# python problem.
+# String methods.
+
+a = "HELLO"
+b = "1234hello"
+c = "1234567890"
+d = "hello everyone"
+e = "1234.344"
+f = "Hello 1234"
+g = "nikhil singh"
+h = " "
+i = "Hey My Name Is Roshan Singh"
