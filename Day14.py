79d37f54514ee3e70a3a556f64be7d17cb28e069
Raushan kumar <sahilsingh22111@gmail.com>
2026-04-04 10:34:39 +0530

    Update Day14.py

--- Day14.py
+++ Day14.py
@@ -6,3 +6,8 @@ print(a,a.isalpha())
 print(g,g.isalpha())
 print(b,b.isalpha())
 
+# isdecimal - Return true if all characters in the string in the decimal.
+print(a,a.isdecimal())
+print(c,c.isdecimal())
+print(e,e.isdecimal())
+
