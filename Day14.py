0f3face17116eb327b66fd80ff4fbcc2ad26fe4a
Raushan kumar <sahilsingh22111@gmail.com>
2026-04-04 10:37:24 +0530

    Update Day14.py

--- Day14.py
+++ Day14.py
@@ -1,6 +1,16 @@
 # Python Questions.
 # String methods.
 
+a = "HELLO"
+b = "1234hello"
+c = "1234567890"
+d = "hello everyone"
+e = "1234.344"
+f = "Hello 1234"
+g = "nikhil singh"
+h = " "
+i = "Hey My Name Is Roshan Singh"
+
 # isalpha - Return true if all characters in the string in the alphabet.
 print(a,a.isalpha())
 print(g,g.isalpha())
