89479698660499137a155d8a27b63c910a0b9c02
Raushan kumar <sahilsingh22111@gmail.com>
2026-04-04 10:33:55 +0530

    Create Day14.py

---
+++ Day14.py
@@ -0,0 +1,8 @@
+# Python Questions.
+# String methods.
+
+# isalpha - Return true if all characters in the string in the alphabet.
+print(a,a.isalpha())
+print(g,g.isalpha())
+print(b,b.isalpha())
+
