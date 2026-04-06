e41cc9e4752f88ba58f74e3f73d5aec33cf6fc13
Raushan kumar <sahilsingh22111@gmail.com>
2026-04-06 22:15:46 +0530

    Update Day15.py

--- Day15.py
+++ Day15.py
@@ -6,3 +6,10 @@ print(a,a.islower())
 print(b,b.islower())
 print(c,c.islower())
 print(d,d.islower())
+
+# isupper - check into string upper case.
+print(a,a.isupper())
+print(b,b.isupper())
+print(c,c.isupper())
+print(d,d.isupper())
+print(e,e.isupper())
