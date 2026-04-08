69a6a58b637f032767e3f8afd8d611f96fe9b2b5
Raushan kumar <sahilsingh22111@gmail.com>
2026-04-08 16:05:30 +0530

    Create Day16.py

---
+++ Day16.py
@@ -0,0 +1,10 @@
+# Python Questions.
+# String Methods.
+
+# endswith() - Return true if the string ends with the specified value.
+a = "i like play cricket"
+print(a.endswith("y",4,11))
+
+# startswith - Return true if the string starts with the specified value.
+b = "i believe in myself"
+print(b.startswith("m",13,16))
