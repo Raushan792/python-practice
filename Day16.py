e86d80154944c74f3e008f51460bb4d810617829
Raushan kumar <sahilsingh22111@gmail.com>
2026-04-08 16:06:10 +0530

    Update Day16.py

--- Day16.py
+++ Day16.py
@@ -8,3 +8,7 @@ print(a.endswith("y",4,11))
 # startswith - Return true if the string starts with the specified value.
 b = "i believe in myself"
 print(b.startswith("m",13,16))
+
+# swap case() - Swap case , lower case becomes upper case and vise versa.
+c = "How Old Are You My Friend"
+print(c.swapcase())
