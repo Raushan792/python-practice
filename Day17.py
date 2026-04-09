3772765cdfda69269ab2258e941917a71cbc76ca
Raushan kumar <sahilsingh22111@gmail.com>
2026-04-09 14:39:49 +0530

    Update Day17.py

--- Day17.py
+++ Day17.py
@@ -6,3 +6,9 @@ s = "proud to be Rajputana"
 x = s.ljust(8)
 print(x,"culture")
 
+# replace - Returns a string where a specified valve is replaced with a
+# specified value.
+r = "i am a hacker.hacker is so smart"
+print(r)
+print(r.replace("hacker", "singh"))
+
