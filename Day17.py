92e215b176154284b932cc9a8839e5d06ed3124e
Raushan kumar <sahilsingh22111@gmail.com>
2026-04-09 14:40:52 +0530

    Update Day17.py

--- Day17.py
+++ Day17.py
@@ -12,3 +12,8 @@ r = "i am a hacker.hacker is so smart"
 print(r)
 print(r.replace("hacker", "singh"))
 
+# rindex - Search the string for a specified value and returns
+# the last position of where it was found.
+w = "Roshan Singh is a computer science student"
+print(w.rindex("computer science"))
+
